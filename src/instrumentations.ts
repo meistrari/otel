7cb62956669971509755db7f86e8a90f801e7596
Matheus Peviani Vellone <matheusvellone@gmail.com>
2025-08-07 16:07:23 -0300

    fix: attempt to override process.release.name before loading fetch instrumentation

--- src/instrumentations.ts
+++ src/instrumentations.ts
@@ -1,13 +1,17 @@
-// https://github.com/oven-sh/bun/issues/3775#issuecomment-3026955059
-process.release.name = 'bun'
-import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch'
-process.release.name = 'node'
 
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 
 const instrumentations = [
   ...getNodeAutoInstrumentations(),
-  new FetchInstrumentation(),
+  await (async () => {
+    // https://github.com/oven-sh/bun/issues/3775#issuecomment-3026955059
+    process.release.name = 'bun'
+    const { FetchInstrumentation } = await import('@opentelemetry/instrumentation-fetch')
+    process.release.name = 'node'
+
+    return new FetchInstrumentation()
+  })(),
+  // new FetchInstrumentation(),
 ]
 
 export default instrumentations
